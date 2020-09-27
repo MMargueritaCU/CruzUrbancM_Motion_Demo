47f53032c147e04296006d2bef359c967b32d8b2
Marguerita Cruz-Urbanc <mmc399@drexel.edu>
2020-09-26 23:22:28 -0400

    continued demo

--- dev/js/demo.js
+++ dev/js/demo.js
@@ -5,6 +5,8 @@ import {gsap} from "gsap";
     Demo Script
 ----------------- */
 
+// gsap call / what do you want to animate / how long should it animate / what should change?
+
 var timeOfAnimation = 3;
 
 gsap.to(".purple-box",{duration: timeOfAnimation,borderRadius: "20"});
